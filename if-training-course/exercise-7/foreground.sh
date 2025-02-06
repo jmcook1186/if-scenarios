1ad1c87f5ab76100525d69e8cfed011b55a4ad36
jmc <33655003+jmcook1186@users.noreply.github.com>
2025-02-06 15:05:56 +0000

    set up e7

---
+++ if-training-course/exercise-7/foreground.sh
@@ -0,0 +1,37 @@
+# Download and install nvm:
+echo "*** Setting up Impact framework environment ***"
+echo
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+
+# *** Installing NodeJS ***
+#
+nvm install 22
+
+# Verify the Node.js version:
+#
+node -v # Should print "v22.13.1".
+nvm current # Should print "v22.13.1".
+
+# Verify npm version:
+#
+npm -v # Should print "10.9.2".
+
+# Installing Impact Framework
+#
+npm install -g npm@11.1.0
+npm install -g @grnsft/if
+
+# *** Downloading manifest files ***"
+#
+wget https://raw.githubusercontent.com/jmcook1186/if-scenarios/refs/heads/main/if-training-course/exercise-6/manifest-example.yml
+wget https://raw.githubusercontent.com/jmcook1186/if-scenarios/refs/heads/main/if-training-course/exercise-6/boilerplate.yml
+
+
+#
+#*** Setup complete! ***
+#
---
+++ if-training-course/exercise-7/step1.md
@@ -0,0 +1,12 @@
+So far, your manifests have only had a single element in the inputs array. However, many applications will actually track usage metrics over time. Therefore inputs will often be a time series.
+
+Try creating multiple entries in your `inputs` array.
+
+Remember, each timestep must have a unique `timestamp` and a `duration`. 
+You can vary the values for each field in each element.
+
+Save your manifest as `manifest.yml` and run it using:
+
+```
+if-run -m manifest.yml -o outputs.yml
+```
